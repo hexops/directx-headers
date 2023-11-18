090483e8463a276ac7f72cb1d11c14c40a1c0996
Stephen Gutekanst <stephen@hexops.com>
2023-11-18 12:36:48 -0700

    add Zig build system
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

---
+++ build.zig
@@ -0,0 +1,21 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const lib = b.addStaticLibrary(.{
+        .name = "directx-headers",
+        .root_source_file = b.addWriteFiles().add("empty.c", ""),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    inline for (.{ "directx", "dxguids", "wsl" }) |subdir| {
+        lib.installHeadersDirectory("include/" ++ subdir, subdir);
+    }
+    b.installArtifact(lib);
+
+    // TODO: consider exposing dxguids?
+    // Rewrite that portion in Zig to avoid C++ compiler?
+}
